549cbf90f4b905bc82b2fcbbcca83e74c6f1d307
Andrey <Sr.Andrey.Rodionov@gmail.com>
2022-07-11 00:08:41 +0300

    creation of layout

---
+++ src/component/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Button, Container, Grid} from "@mui/material";
+
+const Login = () => {
+    return (
+        <Container>
+            <Grid container
+            style={{height: window.innerHeight - 50}}
+                  alignItems={'center'}
+                  justifyContent={'center'}
+            >
+                <Grid style={{width:400, background: 'lightgrey'}}
+                      container
+                      alignItems={'center'}
+                      direction={'column'}
+                >
+                    <box>
+                        <Button variant={"outlined"}>Login via Google</Button>
+                    </box>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Login;
---
+++ src/component/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {AppBar, Button, Grid, Toolbar} from "@mui/material";
+import {NavLink} from "react-router-dom";
+import {LOGIN_ROUTE} from "../utils/consts";
+
+const Navbar = () => {
+    const user = false
+    return (
+        <AppBar color={"secondary"} position="static">
+            <Toolbar variant={"dense"}>
+                <Grid container justifyContent="flex-end">
+                    {user ?
+                        <Button variant={"outlined"}>Logout</Button>
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+
+                            <Button variant={"outlined"}>Login</Button>
+                        </NavLink>
+                    }
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
